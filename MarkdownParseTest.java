acac5d79fe13aa9ee673a603150dba123e24d030
Justin Khant <1kaungminkhant1@gmail.com>
2022-05-30 18:48:22 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import org.junit.Test;
 public class MarkdownParseTest {
@@ -7,5 +11,12 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    @Test
+    public void snippetTest1() throws IOException {
+        Path fileName = Path.of("./lab-report-4-test-1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), 
+             MarkdownParse.getLinks(contents));
+}
 
 }
